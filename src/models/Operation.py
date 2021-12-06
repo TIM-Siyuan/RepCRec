f19a5bb8a5fff7b2611ac0807c5cb1e31fe1a70e
qh2055 <qh2055@nyu.edu>
2021-12-05 21:23:51 -0500

    input

---
+++ src/models/Operation.py
@@ -0,0 +1,10 @@
+class operation:
+
+    def __init__(self, operation_type, Tid, vid, value, sid, time):
+        self.type = operation_type
+        self.Tid = Tid
+        self.vid = vid
+        self.value = value
+        self.sid = sid
+        self.time = time
+
